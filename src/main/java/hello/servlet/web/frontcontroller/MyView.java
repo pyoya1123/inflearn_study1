f7dd10dc52310cc87d227aadc5c99ba447f121a6
jeonseongpyo <pyoya1123@gmail.com>
2024-07-28 22:04:47 +0900

    View 분리 - v2

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,23 @@
+package hello.servlet.web.frontcontroller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyView {
+
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    // jsp로 이동하고 막 이런거를 렌더링 한다고 표현할거임. 뭔가 실제 view가 렌더링 되도록 동자갛는거 httpServletRequest를 받고
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
